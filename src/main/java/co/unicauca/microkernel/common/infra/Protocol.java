2c5c62784322410032dd9a2f22088a641c2262ca
Luis Arango <lmanuelarango97@outlook.es>
2021-04-26 21:57:59 -0500

    Creada clase Protocol

---
+++ src/main/java/co/unicauca/microkernel/common/infra/Protocol.java
@@ -0,0 +1,62 @@
+package co.unicauca.microkernel.common.infra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Luis Arango
+ */
+public class Protocol {
+    private String resource;
+    private String action;
+    private List<Parameter> parameters;
+    private byte[] bytes;
+    
+    /**
+     * Constructor por Defecto
+     */
+    public Protocol() {
+        parameters = new ArrayList<>();
+    }
+    
+    /**
+     * Getter y setter
+     * @return 
+     */
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+    
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Parameter> parameters) {
+        this.parameters = parameters;
+    }
+
+    public void addParameter(String name, String value) {
+        parameters.add(new Parameter(name, value));
+    }
+}
