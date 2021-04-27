7d2a439de894c1a032e0126ed1fd1de06dbbe70b
Luis Arango <lmanuelarango97@outlook.es>
2021-04-26 21:55:51 -0500

    Creada clase Parameter

---
+++ src/main/java/co/unicauca/microkernel/common/infra/Parameter.java
@@ -0,0 +1,35 @@
+package co.unicauca.microkernel.common.infra;
+
+/**
+ *
+ * @author Luis Arango
+ */
+public class Parameter {
+    private String name;
+    private String value;
+
+    public Parameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Parameter() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
